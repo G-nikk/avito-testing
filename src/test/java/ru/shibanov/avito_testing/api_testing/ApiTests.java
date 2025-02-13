de703a51be606a1d5ff48d62255ccf011d07bd79
German <gnshibanov@mail.ru>
2025-02-13 10:54:54 +0300

    Developed post-requesting autotests & refactored others due to postman collection changes

--- src/test/java/ru/shibanov/avito_testing/api_testing/ApiTests.java
+++ src/test/java/ru/shibanov/avito_testing/api_testing/ApiTests.java
@@ -2,6 +2,8 @@ package ru.shibanov.avito_testing.api_testing;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -13,11 +15,16 @@ public class ApiTests {
     private final String ITEM_ID = "7a8fe969-2a57-468e-82c9-1982d22023c5";
     private final String WRONG_ITEM_ID = "7a8fe969-2a57-468e-82c9-1982d22023c4";
 
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = BASE_URL;
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"1", "12", "12345"})
-    public void getAllItemsBySellerId(String sellerId) {
+    public void getAllItemsBySellerIdResponseBodyCheck(String sellerId) {
         RestAssured
-                .get(BASE_URL + "/" + sellerId + "/item")
+                .get("/" + sellerId + "/item")
                 .then()
                 .assertThat()
                 .statusCode(200)
@@ -25,13 +32,13 @@ public class ApiTests {
                 .contentType(ContentType.JSON)
                 .body("sellerId", hasItem(Integer.valueOf(sellerId)))
                 .and()
-                .body(matchesJsonSchemaInClasspath("schemas/GetAllItemsBySellerId.json"));
+                .body(matchesJsonSchemaInClasspath("schemas/GetAllItemsBySellerIdResponse.json"));
     }
 
     @Test
-    public void getItemById() {
+    public void getItemByIdResponseBodyCheck() {
         RestAssured
-                .get(BASE_URL + "/item/" + ITEM_ID)
+                .get("/item/" + ITEM_ID)
                 .then()
                 .assertThat()
                 .statusCode(200)
@@ -42,40 +49,78 @@ public class ApiTests {
 
     @ParameterizedTest
     @ValueSource(strings = {"7a8fe969-2a57-468e-82c9-1982d22023c4", " ", "#"})
-    public void getItemByWrongId() {
+    public void getItemByWrongIdCorrectStatusCodeCheck() {
         RestAssured
-                .get(BASE_URL + "/item/" + WRONG_ITEM_ID)
+                .get("/item/" + WRONG_ITEM_ID)
                 .then()
                 .assertThat()
-                .statusCode(404)
+                .statusCode(400)
                 .and()
                 .contentType(ContentType.JSON)
-                .body("status", equalTo("404"));
+                .body("status", equalTo("400"));
     }
 
     @Test
-    public void getItemWithoutId() {
+    public void getItemWithoutIdCorrectStatusCodeCheck() {
         RestAssured
-                .get(BASE_URL + "/item/")
+                .get("/item/")
                 .then()
                 .assertThat()
-                .statusCode(404)
+                .statusCode(400)
                 .and()
                 .contentType(ContentType.JSON)
-                .body("code", equalTo(404));
+                .body("code", equalTo(400));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"id", "13"})
-    public void getItemByWrongIdFormat(String itemId) {
+    public void getItemByWrongIdFormatCorrectStatusCodeCheck(String itemId) {
         RestAssured
-                .get(BASE_URL + "/item/" + itemId)
+                .get("/item/" + itemId)
                 .then()
                 .assertThat()
-                .statusCode(500)
+                .statusCode(400)
                 .and()
                 .contentType(ContentType.JSON)
-                .body("code", equalTo(500));
+                .body("status", equalTo("400"));
     }
 
+    @Test
+    public void postItemResponseBodyCheck() {
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body(createItemRequestBody())
+                .post("/item")
+                .then()
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("schemas/PostItemResponse.json"));
+    }
+
+    @Test
+    public void postItemCorrectStatusCodeCheck() {
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body(createItemRequestBody())
+                .post("/item")
+                .then()
+                .assertThat()
+                .statusCode(201);
+    }
+
+    private JSONObject createItemRequestBody() {
+        JSONObject statistics = new JSONObject();
+        statistics.put("contacts", 3);
+        statistics.put("likes", 123);
+        statistics.put("viewCount", 12);
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("sellerID", 1234345231);
+        requestBody.put("name", "dsds");
+        requestBody.put("price", 1);
+        requestBody.put("statistics", statistics);
+
+        return requestBody;
+    }
 }
